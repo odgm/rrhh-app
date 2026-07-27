4433e2449ede320bfccca312b152c29f2aa0bd9d
odgm <ordyelizkh@gmail.com>
2026-07-27 15:44:51 -0300

    Formulario de Agregar Empleado con Bootstrap y React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
 import Navegacion from "./plantilla/Navegacion";
+import AgregarEmpleado from "./empleados/AgregarEmpleado";
 
 function App() {
   return (
     <div className="container">
-      <Navegacion/>
-      <ListadoEmpleados/>
+      <BrowserRouter>
+        <Navegacion></Navegacion>
+        <Routes>
+          <Route exact path="/" element={<ListadoEmpleados></ListadoEmpleados>}></Route>
+          <Route exact path="/agregar" element={<AgregarEmpleado></AgregarEmpleado>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
     
   );
