6b0703f3cbdf0b235f1d650b4aca86ef3983995a
odgm <ordyelizkh@gmail.com>
2026-07-27 13:23:11 -0300

    Listar Empleados con React y Axios

--- src/empleados/ListadoEmpleados.js
+++ src/empleados/ListadoEmpleados.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
 
 export default function ListadoEmpleados() {
+
+    const urlBase = "http://localhost:8080/rrhh-app/empleados";
+
+    
+    const[empleados, setEmpleados] = useState([]);
+
+    useEffect( () => {
+        cargarEmpleados();
+    }, [] );
+
+    const cargarEmpleados = async () => {
+        const resultado = await axios.get(urlBase);
+        console.log("Resultado de Cargar Empleados");
+        console.log(resultado.data);
+        setEmpleados(resultado.data);
+    }
+
     return (
         <div className="container">
             <div className="container text-center" style={{ margin: "30px" }}>
