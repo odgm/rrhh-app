b3708b59cd8bb9136207c241620cb5c5e59e824b
odgm <ordyelizkh@gmail.com>
2026-07-28 15:29:44 -0300

    Buscar por id

--- src/empleados/ListadoEmpleados.js
+++ src/empleados/ListadoEmpleados.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react'
 import { NumericFormat } from 'react-number-format';
+import { Link } from 'react-router-dom';
 
 export default function ListadoEmpleados() {
 
@@ -32,23 +33,30 @@ export default function ListadoEmpleados() {
                         <th scope="col">Empleado</th>
                         <th scope="col">Departamento</th>
                         <th scope="col">Sueldo</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                    //Iteramos el arreglo de empleados
-                    empleados.map((empleado, indice) => (
-                        <tr key="{indice}">
-                            <th scope="row">{empleado.idEmpleado}</th>
-                            <td>{empleado.nombre}</td>
-                            <td>{empleado.departamento}</td>
-                            <td><NumericFormat value={empleado.sueldo}
-                                displayType={'text'}
-                                thousandSeparator=',' prefix={'$'}
-                                decimalScale={2} fixedDecimalScale/>
-                            </td>
-                        </tr>
-                    ))
+                        //Iteramos el arreglo de empleados
+                        empleados.map((empleado, indice) => (
+                            <tr key="{indice}">
+                                <th scope="row">{empleado.idEmpleado}</th>
+                                <td>{empleado.nombre}</td>
+                                <td>{empleado.departamento}</td>
+                                <td><NumericFormat value={empleado.sueldo}
+                                    displayType={'text'}
+                                    thousandSeparator=',' prefix={'$'}
+                                    decimalScale={2} fixedDecimalScale />
+                                </td>
+                                <td className="text-center">
+                                    <div>
+                                        <Link to={`/editar/${empleado.idEmpleado}`}
+                                            className="btn btn-warning btn-sm me-3">Editar</Link>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))
                     }
                 </tbody>
             </table>
